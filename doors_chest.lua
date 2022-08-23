7f68d51e2b15a5cb5587a2bf379288bfe3ca8005
tenplus1 <kinsellaja@yahoo.com>
2022-08-23 08:32:52 +0100

    chest names only use alphanumerical and spaces

--- doors_chest.lua
+++ doors_chest.lua
@@ -740,6 +740,11 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 
 	elseif fields.protect_name or fields.protect_rename then
 
+		if fields.protect_name ~= string.match(fields.protect_name, "[%w%s_-]+")
+		or fields.protect_name:len() > 35 then
+			return
+		end
+
 		-- change chest infotext to display name
 		if fields.protect_name ~= "" then
 
